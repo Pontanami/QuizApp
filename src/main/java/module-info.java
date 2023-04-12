3b03ec2e0ca8aceff3d4fb250c5ca487ff5eb636
Pontanami <pontusen@student.chalmers.se>
2023-04-12 16:14:17 +0200

    firebase pom dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.quizapp {
     requires javafx.controls;
     requires javafx.fxml;
+    requires google.cloud.firestore;
+    requires firebase.admin;
 
 
     opens com.example.quizapp to javafx.fxml;
