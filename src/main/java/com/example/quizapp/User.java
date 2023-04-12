d6d12671ef3d6d6e78d84a3b70b21c425485f179
Gustav Berndtzen <gustav.berndtzen@gmail.com>
2023-04-12 21:57:23 +0200

    added User class to work with object mapping

--- src/main/java/com/example/quizapp/User.java
+++ src/main/java/com/example/quizapp/User.java
@@ -2,10 +2,14 @@ package com.example.quizapp;
 
 public class User {
 
-    private String name;
-    private String email;
-    private String password;
+    public String name;
+    public String email;
+    public String password;
 
+
+    public User(){
+
+    }
     public User(String name, String email, String password){
         this.name = name;
         this.email = email;
