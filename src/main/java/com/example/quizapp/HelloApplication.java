688d1260f89556cf5abbe743566999f30f774ef1
Svanstedt <nils.svanstedt@gmail.com>
2023-04-13 17:59:05 +0200

    Working flashcard command line application

--- src/main/java/com/example/quizapp/HelloApplication.java
+++ src/main/java/com/example/quizapp/HelloApplication.java
@@ -7,6 +7,8 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class HelloApplication extends Application {
@@ -21,47 +23,92 @@ public class HelloApplication extends Application {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        List<Flashcard> flashCards = new ArrayList<>();
+        boolean running = true;
+        while (running) {
+            System.out.println("1 - Create a flashcard");
+            System.out.println("2 - Show flashcards");
+            System.out.println("3 - Delete flashcard");
+            System.out.println("Q - Exit");
+            System.out.print("Pick an option: " );
 
-        System.out.println("1 - Create flashcards");
-        System.out.println("2 - Show flashcards");
-        System.out.println("3 - Delete flashcard");
-        System.out.println("Q - Exit");
+            String option = in.nextLine();
+            System.out.println();
+
+            switch (option) {
+                case "1":
+                    System.out.print("Enter your question: ");
+                    String question = in.nextLine();
+
+                    System.out.print("Enter your answer: ");
+                    String answer = in.nextLine();
 
 
-        System.out.print("Create your question: ");
-        String question = in.nextLine();
+                    flashCards.add(new Flashcard(question, answer));
+                    break;
+                case "2":
+                    if (flashCards.size() == 0) {
+                        System.out.println("No flashcards added.");
+                        break;
+                    }
+                    boolean viewingFlashCards = true;
+                    int currentFlashCard = 0;
+                    while (viewingFlashCards) {
+                        System.out.println("1 - Show question");
+                        System.out.println("2 - Show answer");
+                        System.out.println("3 - Go to previous flashcard");
+                        System.out.println("4 - Go to next flashcard");
+                        System.out.println("Q - Exit ");
+                        System.out.print("Pick an option: " );
 
-        System.out.print("And the answer: ");
-        String answer = in.nextLine();
+                        option = in.nextLine();
+                        System.out.println();
 
-        clearConsole();
+                        switch (option) {
+                            case "1":
+                                System.out.println(flashCards.get(currentFlashCard).getQuestion());
+                                break;
+                            case "2":
+                                System.out.println(flashCards.get(currentFlashCard).getAnswer());
+                                break;
+                            case "3":
+                                if (currentFlashCard != 0) {
+                                    currentFlashCard--;
+                                } else {
+                                    currentFlashCard = flashCards.size() - 1;
+                                }
+                                break;
+                            case "4":
+                                if (currentFlashCard != flashCards.size() - 1) {
+                                    currentFlashCard++;
+                                } else {
+                                    currentFlashCard = 0;
+                                }
+                                break;
+                            case "Q":
+                                viewingFlashCards = false;
+                                break;
+                        }
+                        System.out.println();
 
-        Flashcard flashcard = new Flashcard(question, answer);
-
-        System.out.println(flashcard.getQuestion());
-
-        System.out.println("Press S to see the answer");
-        String choice = in.nextLine();
-
-        if (choice.equalsIgnoreCase("s")){
-            System.out.println(flashcard.getAnswer());
+                    }
+                    break;
+                case "3":
+                    System.out.println("Which flashcard do you want to delete?");
+                    for (int i = 0; i < flashCards.size(); i++) {
+                    System.out.println(i + " - " + flashCards.get(i).getQuestion());
+                    }
+                    int flashcard = Integer.parseInt(in.nextLine());
+                    flashCards.remove(flashcard);
+                    break;
+                case "Q":
+                    running = false;
+                    break;
+            }
+            System.out.println();
         }
-
 
         launch();
     }
 
-    public static void clearConsole() {
-        try {
-            final String os = System.getProperty("os.name");
-            if (os.contains("Windows")) {
-                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-            } else {
-                System.out.print("33[H33[2J");
-                System.out.flush();
-            }
-        } catch (IOException | InterruptedException ex) {
-            ex.printStackTrace();
-        }
-    }
 }
