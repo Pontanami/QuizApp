237a09cfcf5198f683f0b54fb8dcddc819bfce36
Wiz-Omar <oyounes57@gmail.com>
2023-04-14 14:06:30 +0200

    Completed tests

--- src/main/java/com/example/quizapp/HelloApplication.java
+++ src/main/java/com/example/quizapp/HelloApplication.java
@@ -2,6 +2,8 @@ package com.example.quizapp;
 
 import com.example.quizapp.multiChoice.MultiChoice;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,15 +13,19 @@ import java.util.Scanner;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        /* FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
-        stage.show(); */
+        stage.show();
     }
 
     public static void main(String[] args) {
-        //launch();
+        launch();
+
+    }
+
+    private static void multiChoiceFlow(){
         ArrayList<MultiChoice> questions = new ArrayList<>();
         String[] options = {"Create a question", "Take a quiz", "Exit"};
 
@@ -80,6 +86,5 @@ public class HelloApplication extends Application {
         }
 
         System.exit(0);
-
     }
 }
