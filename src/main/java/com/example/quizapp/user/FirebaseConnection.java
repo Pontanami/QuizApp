d0c95ef317ecde1888075e26992797dee8d08fea
Pontanami <pontusen@student.chalmers.se>
2023-04-26 14:18:00 +0200

    FirebaseConnection javadoc

--- src/main/java/com/example/quizapp/user/FirebaseConnection.java
+++ src/main/java/com/example/quizapp/user/FirebaseConnection.java
@@ -8,6 +8,10 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+/**
+ * This class is a singleton used to create a single connection to the Firebase database since only a
+ * single instance of FirebaseApp to the same url may be created.
+ */
 public class FirebaseConnection {
 
     private static FirebaseConnection instance = null;
