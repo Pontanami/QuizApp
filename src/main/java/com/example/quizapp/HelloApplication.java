c365d4da63465f69eb51c147a15f0607beb24dcd
aalexpersson <alex.persson0408@gmail.com>
2023-04-26 16:32:39 +0200

    Added views and controllers for login and profile page

--- src/main/java/com/example/quizapp/HelloApplication.java
+++ src/main/java/com/example/quizapp/HelloApplication.java
@@ -84,7 +84,7 @@ public class HelloApplication extends Application {
 
         }*/
 
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/fxml/createQuiz.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/fxml/login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1500, 700);
         stage.setTitle("Hello!");
         stage.setScene(scene);
---
+++ src/main/java/com/example/quizapp/controllers/LoginController.java
@@ -0,0 +1,28 @@
+package com.example.quizapp.controllers;
+
+import com.example.quizapp.user.FirebaseUserRepository;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginController extends AnchorPane{
+    FirebaseUserRepository ur = FirebaseUserRepository.getAuth();
+    @FXML
+    AnchorPane rootpane;
+
+    @FXML
+    TextField emailField;
+    @FXML
+    TextField passwordField;
+    @FXML
+    Button loginBtn;
+
+    //Kolla borderpane
+    @FXML
+    public void login(){
+        String email = emailField.getText();
+        String pw = passwordField.getText();
+        ur.loginUser(email, pw);
+    }
+}
