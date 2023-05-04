fa1b3a316407aaaffdfd3261fe1a237fa27214c0
Wiz-Omar <oyounes57@gmail.com>
2023-05-05 00:02:22 +0200

    Updated tests to adhere to the decided DOD

--- src/test/java/com/example/quizapp/multiChoice/MultiChoiceTest.java
+++ src/test/java/com/example/quizapp/multiChoice/MultiChoiceTest.java
@@ -2,6 +2,7 @@
 package com.example.quizapp.multiChoice;
 
 import com.example.quizapp.hints.EliminateChoiceHint;
+import com.example.quizapp.hints.IHint;
 import com.example.quizapp.quiz.multichoice.MultiChoice;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -16,17 +17,31 @@ class MultiChoiceTest {
     @CsvSource({"What is the water's freezing temp in C, 0, 212, 100, 32, 1",
                 "What is the earth's diameter in km, 12352, 12742, 14576, 15667, 2"})
     void isCorrectlySetup(String questionEx, String answer1, String answer2, String answer3, String answer4) {
-        //externalAns represents an answer that is not one of the four choices which is expected to throw and
-        List<String> expectedChoices = new ArrayList<>();
-        expectedChoices.add(answer1);
-        expectedChoices.add(answer2);
-        expectedChoices.add(answer3);
-        expectedChoices.add(answer4);
+        List<String> expectedChoices = new ArrayList<>(){
+            {
+                add(answer1);
+                add(answer2);
+                add(answer3);
+                add(answer4);
+            }
+        };
+
         MultiChoice mul = new MultiChoice(questionEx, answer1, expectedChoices, new EliminateChoiceHint(expectedChoices, answer1));
         Assertions.assertArrayEquals(expectedChoices.toArray(), mul.getChoices().toArray());
 
         Assertions.assertEquals(answer1, mul.getAnswer());
         Assertions.assertEquals(questionEx, mul.getQuestion());
+
+        List<String> hint = mul.showHint();
+        Assertions.assertEquals(mul.getChoices().size()/2, hint.size());
+
+        int counter = 0;
+        for (String choice: hint){
+            if (mul.getChoices().contains(choice)){
+                counter++;
+            }
+        }
+        Assertions.assertEquals(mul.getChoices().size()/2, counter);
     }
 }
 
