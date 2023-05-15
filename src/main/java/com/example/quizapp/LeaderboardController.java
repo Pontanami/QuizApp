c6aa41b92eceb2281108a1b123d6f8383be2b60f
Pontanami <pontusen@student.chalmers.se>
2023-05-15 10:59:02 +0200

    added fxml constructor

--- src/main/java/com/example/quizapp/LeaderboardController.java
+++ src/main/java/com/example/quizapp/LeaderboardController.java
@@ -1,4 +1,22 @@
 package com.example.quizapp;
 
+import javafx.fxml.FXMLLoader;
+
+import java.io.IOException;
+
 public class LeaderboardController {
+
+    public LeaderboardController() {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/Leaderboard.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (
+                IOException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+
 }
